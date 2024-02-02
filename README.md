fdd017bd04b139d6e385b4e0c3160a9a2ec82782
hibana2077 <hibana2077@gmail.com>
2024-02-02 17:59:10 +0800

    Delete src/main.py and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# find_the_stock
-用機器學習的方法尋找台股飆股                                                                                           --這句話真的有夠白癡
+<!--
+ * @Author: hibana2077 hibana2077@gmail.com
+ * @Date: 2024-02-02 17:40:31
+ * @LastEditors: hibana2077 hibana2077@gmail.com
+ * @LastEditTime: 2024-02-02 17:41:04
+ * @FilePath: /nexus/README.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+# NEXUS
---
+++ src/web/tailwind.config.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: hibana2077 hibana2077@gmail.com
+ * @Date: 2024-02-02 17:50:59
+ * @LastEditors: hibana2077 hibana2077@gmail.com
+ * @LastEditTime: 2024-02-02 17:52:46
+ * @FilePath: /nexus/src/web/tailwind.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{svelte,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
